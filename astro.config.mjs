1173f515cab1afa6319d07e733251965d1b34d72
lutfiangga <lutfiangga2908@gmail.com>
2026-02-05 21:44:06 +0700

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,14 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 import "dotenv/config"
 import cloudflare from '@astrojs/cloudflare';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://nontn.web.id',
     output: 'server',
     adapter: cloudflare(),
+    integrations: [sitemap()],
     vite: {
         plugins: [tailwindcss()]
     }
